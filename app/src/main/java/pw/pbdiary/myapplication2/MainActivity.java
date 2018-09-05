5d6f8711d0278d5f9603e45944438afca3c758fb
윤정민(종이상자) <lego37yoon@outlook.com>
2018-09-05 19:31:11 +0900

    Initial commit

---
+++ app/src/main/java/pw/pbdiary/myapplication2/MainActivity.java
@@ -0,0 +1,23 @@
+package pw.pbdiary.myapplication2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/pw/pbdiary/myapplication2/MenuActivity.java
@@ -0,0 +1,26 @@
+package pw.pbdiary.myapplication2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name","paperbox");
+                setResult(RESULT_OK,intent);
+            }
+        });
+    }
+}
