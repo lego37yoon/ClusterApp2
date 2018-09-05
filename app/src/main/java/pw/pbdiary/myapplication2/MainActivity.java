6e07c1935def3e8aef951465dec6d574147c8e6a
윤정민(종이상자) <lego37yoon@outlook.com>
2018-09-05 20:35:00 +0900

    빼먹은 게 있지만 나중에 하기로

--- app/src/main/java/pw/pbdiary/myapplication2/MainActivity.java
+++ app/src/main/java/pw/pbdiary/myapplication2/MainActivity.java
@@ -27,4 +27,40 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("data", data);
         startActivityForResult(intent, REQUEST_CODE_MENU);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"onStart",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"onStop",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"onDestroy",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"onPause",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"onResume",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"onRestart",Toast.LENGTH_LONG).show();
+    }
 }
