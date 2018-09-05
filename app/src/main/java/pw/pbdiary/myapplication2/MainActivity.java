0dfb4834b32beed7152c99035d19bbd47599e622
윤정민(종이상자) <lego37yoon@outlook.com>
2018-09-05 19:39:21 +0900

    maeari 개발할 때 유용한것: Activity 간 Intent로 데이터 주고받기

--- app/src/main/java/pw/pbdiary/myapplication2/MainActivity.java
+++ app/src/main/java/pw/pbdiary/myapplication2/MainActivity.java
@@ -1,7 +1,7 @@
 package pw.pbdiary.myapplication2;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUEST_CODE_MENU = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.textView);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_MENU);
+    }
+    //결과를 받으면 오는 것!
+    //Android Studio > MainActivity.java 열기 > 오른쪽클릭 > Generate > Override ... > onActivtyResult 선택.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_MENU) {
+            //menuActivity가 보내준 것임을 알게 됨!
+            Toast.makeText(getApplicationContext(), "OnActivityResult 호출됨"+requestCode+"결과코드", Toast.LENGTH_LONG).show();
+        }
     }
 }
